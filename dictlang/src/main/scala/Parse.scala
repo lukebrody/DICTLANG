e7b4f5f5a72c50248cd99e194bc49e0418300534
Luke Brody <lukebrody@pavonine.co>
2021-10-13 19:58:15 -0700

    ParseResult => Parse

--- dictlang/src/main/scala/Parse.scala
+++ dictlang/src/main/scala/Parse.scala
@@ -1,15 +1,13 @@
-sealed trait ParseResult[+A] {
-  self =>
+sealed trait Parsing[+A] {
+  def map[B](f: A => B): Parsing[B]
+  def flatMap[B](f: A => Parsing[B]): Parsing[B]
 
-  def map[B](f: A => B): ParseResult[B]
-  def flatMap[B](f: A => ParseResult[B]): ParseResult[B]
+  def orElse[B >: A](other: Parsing[B]): Parsing[B]
 
-  def orElse[B >: A](other: ParseResult[B]): ParseResult[B]
+  def pop[T <: Token](token: T): Parsing[T]
+  def popName(): Parsing[Name]
 
-  def pop[T <: Token](token: T): ParseResult[T]
-  def popName(): ParseResult[Name]
-
-  def mapTwo[A, B](a: ParseResult[_] => ParseResult[A], b: ParseResult[_] => ParseResult[B]): ParseResult[(A, B)] = {
+  def mapTwo[A, B](a: Parsing[_] => Parsing[A], b: Parsing[_] => Parsing[B]): Parsing[(A, B)] = {
     val first = a(this)
     val second = b(first)
     first.flatMap { firstResult =>
@@ -20,17 +18,17 @@ sealed trait ParseResult[+A] {
   }
 }
 
-case class Success[+A](ast: A, rest: Seq[Token]) extends ParseResult[A] {
-  override def map[B](f: A => B): ParseResult[B] = Success[B](f(ast), rest)
-  override def flatMap[B](f: A => ParseResult[B]): ParseResult[B] = f(ast)
-  override def orElse[B >: A](other: ParseResult[B]): ParseResult[B] = this
+case class Success[+A](ast: A, rest: Seq[Token]) extends Parsing[A] {
+  override def map[B](f: A => B): Parsing[B] = Success[B](f(ast), rest)
+  override def flatMap[B](f: A => Parsing[B]): Parsing[B] = f(ast)
+  override def orElse[B >: A](other: Parsing[B]): Parsing[B] = this
 
-  override def pop[T <: Token](token: T): ParseResult[T] = rest match {
+  override def pop[T <: Token](token: T): Parsing[T] = rest match {
     case Seq(`token`, rest @ _*) => Success(token, rest)
     case _                       => Failure
   }
 
-  override def popName(): ParseResult[Name] = rest match {
+  override def popName(): Parsing[Name] = rest match {
     case Seq(Name(name), rest @ _*) => Success(Name(name), rest)
     case _                          => Failure
   }
@@ -40,13 +38,13 @@ object Success {
   def unapply[A <: AST](arg: Success[A]): Option[(A, Seq[Token])] = Some((arg.ast, arg.rest))
 }
 
-case object Failure extends ParseResult[Nothing] {
-  override def map[B](f: Nothing => B): ParseResult[B] = Failure
-  override def flatMap[B](f: Nothing => ParseResult[B]): ParseResult[B] = Failure
-  override def orElse[B >: Nothing](other: ParseResult[B]): ParseResult[B] = other
+case object Failure extends Parsing[Nothing] {
+  override def map[B](f: Nothing => B): Parsing[B] = Failure
+  override def flatMap[B](f: Nothing => Parsing[B]): Parsing[B] = Failure
+  override def orElse[B >: Nothing](other: Parsing[B]): Parsing[B] = other
 
-  override def pop[T <: Token](token: T): ParseResult[T] = Failure
-  override def popName(): ParseResult[Name] = Failure
+  override def pop[T <: Token](token: T): Parsing[T] = Failure
+  override def popName(): Parsing[Name] = Failure
 
 }
 
@@ -57,29 +55,29 @@ sealed trait Pattern extends AST
 case class Definition(name: String) extends Pattern
 
 object Definition {
-  def parse(in: ParseResult[_]): ParseResult[Definition] =
+  def parse(in: Parsing[_]): Parsing[Definition] =
     in.mapTwo(_.pop(Apostrophe), _.popName()).map { case (_, Name(name)) => Definition(name) }
 }
 
 case class Bind(name: String) extends Pattern
 
 object Bind {
-  def parse(in: ParseResult[_]): ParseResult[Bind] =
+  def parse(in: Parsing[_]): Parsing[Bind] =
     in.mapTwo(_.pop(Backtick), _.popName()).map { case (_, Name(name)) => Bind(name) }
 }
 
 case class Symbol(name: String) extends Pattern
 
 object Symbol {
-  def parse(in: ParseResult[_]): ParseResult[Symbol] = in.popName().map { case Name(name) => Symbol(name) }
+  def parse(in: Parsing[_]): Parsing[Symbol] = in.popName().map { case Name(name) => Symbol(name) }
 }
 
 object Dict {
-  def parse[K, V](key: ParseResult[_] => ParseResult[K], value: ParseResult[_] => ParseResult[V])(
-      in: ParseResult[_]
-  ): ParseResult[Seq[(K, V)]] = {
+  def parse[K, V](key: Parsing[_] => Parsing[K], value: Parsing[_] => Parsing[V])(
+      in: Parsing[_]
+  ): Parsing[Seq[(K, V)]] = {
 
-    def parsePairs(in: ParseResult[_]): ParseResult[Seq[(K, V)]] = {
+    def parsePairs(in: Parsing[_]): Parsing[Seq[(K, V)]] = {
       val row = in.mapTwo(_.mapTwo(key, _.pop(Colon)), _.mapTwo(value, _.pop(Comma))).map { case ((k, _), (v, _)) =>
         Seq((k, v))
       }
@@ -98,13 +96,13 @@ object Dict {
 case class PatternDict(dict: Map[Pattern, Pattern]) extends Pattern
 
 object PatternDict {
-  def parse(in: ParseResult[_]): ParseResult[PatternDict] =
+  def parse(in: Parsing[_]): Parsing[PatternDict] =
     Dict.parse(Pattern.parse, Pattern.parse)(in).map { result => PatternDict(result.toMap) }
 }
 
 object Pattern {
-  def parse(in: ParseResult[_]): ParseResult[Pattern] = {
-    val dict: ParseResult[Pattern] = PatternDict.parse(in)
+  def parse(in: Parsing[_]): Parsing[Pattern] = {
+    val dict: Parsing[Pattern] = PatternDict.parse(in)
     dict orElse Definition.parse(in) orElse Bind.parse(in) orElse Symbol.parse(in)
   }
 }
