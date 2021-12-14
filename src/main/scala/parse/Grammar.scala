991147ca5f4a5a250712620485d5db131d7a10b3
Luke Brody <lukebrody@pavonine.co>
2021-12-14 11:02:53 -0800

    Allow annotating the AST

--- src/main/scala/parse/Grammar.scala
+++ src/main/scala/parse/Grammar.scala
@@ -2,14 +2,16 @@ package parse
 
 import tokenize.*
 
-sealed trait Value
-object Value {
+val parsingGrammar = new Grammar[Unit]
 
+import parsingGrammar._
+
+object Value {
   private def dotAfter(left: Parsing[Value]): Parsing[DotExpression | SubscriptExpression] = {
     left.flatMap { l =>
       left
         .mapTwo(
-          _.mapTwo(_.pop(Dot), Access.parse).map { case (_, right) => DotExpression(l, right) },
+          _.mapTwo(_.pop(Dot), AccessParsing.parse).map { case (_, right) => DotExpression(l, right, ()) },
           dotExpr => dotAfter(dotExpr) orElse dotExpr
         )
         .map(_._2)
@@ -21,7 +23,7 @@ object Value {
       left
         .mapTwo(
           _.mapTwo(_.mapTwo(_.pop(OpenSquareBracket), parse), _.pop(CloseSquareBracket)).map { case ((_, right), _) =>
-            SubscriptExpression(l, right)
+            SubscriptExpression(l, right, ())
           },
           subscriptExpr => dotAfter(subscriptExpr) orElse subscriptAfter(subscriptExpr) orElse subscriptExpr
         )
@@ -30,91 +32,74 @@ object Value {
   }
 
   def parse(in: Parsing[_]): Parsing[Value] = {
-    val term = Reference.parse(in) orElse ValueDict.parse(in)
+    val term = ReferenceParsing.parse(in) orElse ValueDictParsing.parse(in)
     dotAfter(term) orElse subscriptAfter(term) orElse term
   }
 }
 
-case class DotExpression(left: Value, right: Access) extends Value
-case class SubscriptExpression(left: Value, right: Value) extends Value
-
-case class Reference(name: String) extends Value
-object Reference {
-  def parse(in: Parsing[_]): Parsing[Reference] = in.popName().map { case Name(name) => Reference(name) }
+object ReferenceParsing {
+  def parse(in: Parsing[_]): Parsing[Reference] = in.popName().map { case Name(name) => Reference(name, ()) }
 }
 
-case class Access(name: String)
-object Access {
-  def parse(in: Parsing[_]): Parsing[Access] = in.popName().map { case Name(name) => Access(name) }
+object AccessParsing {
+  def parse(in: Parsing[_]): Parsing[Access] = in.popName().map { case Name(name) => Access(name, ()) }
 }
 
-case class ValueDict(entries: Seq[ValueDict.Entry]) extends Value
-object ValueDict {
+object ValueDictParsing {
 
-  sealed trait Entry
-
-  case class DefineEntry(define: Define, value: Value) extends Entry
   object DefineEntry {
-    def parse(in: Parsing[_]): Parsing[DefineEntry] = in.mapTwo(Define.parse, _.mapTwo(_.pop(Colon), Value.parse)).map {
-      case (define, (_, value)) => DefineEntry(define, value)
-    }
+    def parse(in: Parsing[_]): Parsing[ValueDict.DefineEntry] =
+      in.mapTwo(DefineParsing.parse, _.mapTwo(_.pop(Colon), Value.parse)).map { case (define, (_, value)) =>
+        ValueDict.DefineEntry(define, value)
+      }
   }
 
-  case class MatchEntry(pattern: MatchDict, value: Value) extends Entry
   object MatchEntry {
-    def parse(in: Parsing[_]): Parsing[MatchEntry] =
-      in.mapTwo(MatchDict.parse, _.mapTwo(_.pop(DoubleArrow), Value.parse)).map { case (pattern, (_, value)) =>
-        MatchEntry(pattern, value)
+    def parse(in: Parsing[_]): Parsing[ValueDict.MatchEntry] =
+      in.mapTwo(MatchDictParsing.parse, _.mapTwo(_.pop(DoubleArrow), Value.parse)).map { case (pattern, (_, value)) =>
+        ValueDict.MatchEntry(pattern, value)
       }
   }
 
   def parse(in: Parsing[_]): Parsing[ValueDict] = {
     Dict
-      .parse[Entry](in, OpenBracket, CloseBracket) { rowIn =>
+      .parse[ValueDict.Entry](in, OpenBracket, CloseBracket) { rowIn =>
         DefineEntry.parse(rowIn) orElse MatchEntry.parse(rowIn)
       }
-      .map(ValueDict(_))
+      .map(ValueDict(_, ()))
   }
 }
 
-case class Define(name: String)
-object Define {
-  def parse(in: Parsing[_]): Parsing[Define] = in.popName().map { case Name(name) => Define(name) }
+object DefineParsing {
+  def parse(in: Parsing[_]): Parsing[Define] = in.popName().map { case Name(name) => Define(name, ()) }
 }
 
-sealed trait Match
 object Match {
   def parse(in: Parsing[_]): Parsing[Match] = {
-    MatchDict.parse(in) orElse Bind.parse(in)
+    MatchDictParsing.parse(in) orElse BindParsing.parse(in)
   }
 }
 
-case class Bind(name: String) extends Match
-object Bind {
+object BindParsing {
   def parse(in: Parsing[_]): Parsing[Bind] =
     in.popName().map { case Name(name) =>
-      Bind(name)
+      Bind(name, ())
     }
 }
 
-case class MatchDict(entries: Seq[MatchDict.Entry]) extends Match
-object MatchDict {
-
-  case class Entry(left: Extract, right: Match)
-
+object MatchDictParsing {
   def parse(in: Parsing[_]): Parsing[MatchDict] = {
     Dict
-      .parse[Entry](in, OpenSquareBracket, CloseSquareBracket) { rowIn =>
-        rowIn.mapTwo(Extract.parse, _.mapTwo(_.pop(SingleArrow), Match.parse)).map { case (left, (_, right)) =>
-          Entry(left, right)
+      .parse[MatchDict.Entry](in, OpenSquareBracket, CloseSquareBracket) { rowIn =>
+        rowIn.mapTwo(ExtractParsing.parse, _.mapTwo(_.pop(SingleArrow), Match.parse)).map { case (left, (_, right)) =>
+          MatchDict.Entry(left, right)
         }
       }
-      .map(MatchDict(_))
+      .map(MatchDict(_, ()))
   }
 }
 
-case class Extract(name: String)
-object Extract {
+object ExtractParsing {
   def parse(in: Parsing[_]): Parsing[Extract] =
-    in.popName().map { case Name(name) => Extract(name) }
+    in.popName().map { case Name(name) => Extract(name, ()) }
 }
---
+++ src/main/scala/parse/Implementation.scala
@@ -0,0 +1,34 @@
+package parse
+
+class Grammar[A] {
+
+  sealed class AST(info: A)
+
+  sealed trait Value
+  case class DotExpression(left: Value, right: Access, info: A) extends AST(info) with Value
+  case class SubscriptExpression(left: Value, right: Value, info: A) extends AST(info) with Value
+
+  case class Reference(name: String, info: A) extends AST(info) with Value
+  case class Access(name: String, info: A) extends AST(info)
+
+  case class ValueDict(entries: Seq[ValueDict.Entry], info: A) extends AST(info) with Value
+
+  object ValueDict {
+    sealed trait Entry
+    case class DefineEntry(define: Define, value: Value) extends Entry
+    case class MatchEntry(pattern: MatchDict, value: Value) extends Entry
+  }
+
+  case class Define(name: String, info: A) extends AST(info)
+
+  sealed trait Match
+
+  case class Bind(name: String, info: A) extends AST(info) with Match
+
+  case class MatchDict(entries: Seq[MatchDict.Entry], info: A) extends AST(info) with Match
+  object MatchDict {
+    case class Entry(left: Extract, right: Match)
+  }
+
+  case class Extract(name: String, info: A) extends AST(info)
+}
